606b8d481a0603dbac9ab00ecc3355ab12100013
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-11-14 22:40:58 +0100

    Solved Day 12 A & B

--- Source/AoC.Solutions/Y2015/D12.cs
+++ Source/AoC.Solutions/Y2015/D12.cs
@@ -3,47 +3,101 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AoC.Solutions.Y2015
 {
     public class D12 : AoCPuzzle
     {
-        public D12() : base( 2015, 12)
+        public D12() : base(2015, 12)
         { }
 
 
         public override object SolvePuzzleA(string input)
         {
-            string pattern = @"(-{0,1}\d{1,10})";
-            Match matches = Regex.Match(input, pattern);
-
-            
-            int val = 0;
-            while (true)
-            {
-                Match v = matches.NextMatch();
-                if (!v.Success) break;
-
-                val += int.Parse(v.Value);
-
-                matches = v;
-            }
-
-
-
-
-            return val;
+            int sum = 0;
+            JsonNode? jsonNode = JsonNode.Parse(input);
+            ResolveJson(jsonNode, ref sum, false);
+            return sum;
         }
-
         public override object SolvePuzzleB(string input)
         {
-            return 0;
+            int sum = 0;
+            JsonNode? jsonNode = JsonNode.Parse(input);
+            ResolveJson(jsonNode, ref sum, true);
+            return sum;
+        }
+        
+        private static void ResolveJson(JsonNode? node, ref int sum, bool handleRed)
+        {
+            switch (node.GetType().Name)
+            {
+                case nameof(JsonArray):
+                    foreach (JsonNode? jNode in node.AsArray())
+                    {
+                        switch (jNode)
+                        {
+                            case JsonObject or JsonArray:
+
+
+                                ResolveJson(jNode, ref sum, handleRed);
+                                break;
+                            case JsonValue jVal:
+                                {
+                                    if (jVal.TryGetValue(out int value))
+                                    {
+                                        sum += value;
+                                    }
+                                    break;
+                                }
+                        }
+                    }
+                    break;
+                case nameof(JsonObject):
+                    if (handleRed)
+                    {
+                        if (node.AsObject().Any(x => x.Value is JsonValue val && val.TryGetValue(out string? warn) && warn == "red"))
+                            return;
+                    }
+
+                    foreach (KeyValuePair<string, JsonNode?> jKvp in node.AsObject())
+                    {
+                        switch (jKvp.Value)
+                        {
+                            case JsonObject or JsonArray:
+                                ResolveJson(jKvp.Value, ref sum, handleRed);
+                                break;
+                            case JsonValue jVal:
+                                {
+                                    if (jVal.TryGetValue(out string? stValue) && handleRed)
+                                    {
+                                        if (stValue == "red")
+                                        {
+                                            return;
+                                        }
+                                    }
+
+                                    if (jVal.TryGetValue(out int value))
+                                    {
+                                        sum += value;
+                                    }
+                                    break;
+                                }
+                        }
+                    }
+
+                    break;
+            }
         }
 
 
 
-       
+
+
+
+
     }
 }
---
+++ Source/Aoc.Tests/Y2015/Day12.cs
@@ -0,0 +1,30 @@
+﻿using AoC.Solutions.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC.Solutions;
+
+namespace Aoc.Tests.Y2015
+{
+    public class Day12 : DefaultAoCDay
+    {
+
+        public Day12() : base(File.ReadAllText("Inputs\\Y2015D12.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2015.D12();
+        }
+
+        [Test]
+        public override void SampleTestA()
+        { }
+
+        [Test]
+        public override void SampleTestB()
+        { }
+
+    }
+}
