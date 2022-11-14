54976ea6156e700c62ef48ac4498b0d8e51bd5c5
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-11-14 23:23:10 +0100

    Solved Day 13

---
+++ Source/AoC.Solutions/Y2015/D13.cs
@@ -0,0 +1,137 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using SpaceStation.Utilities.LINQ;
+
+namespace AoC.Solutions.Y2015
+{
+    public class D13 : AoCPuzzle
+    {
+        public D13() : base(2015, 13)
+        { }
+
+
+        // 537 is too low
+
+        public override object SolvePuzzleA(string input)
+        {
+            List<PersonHappiness> persons = GeneratePersonList(input).ToList();
+            List<IEnumerable<string>> permutations = GetPermutations(persons);
+            return GenerateSeatMap(permutations, persons).Max(x => x.Happiness);
+
+        }
+        public override object SolvePuzzleB(string input)
+        {
+            List<PersonHappiness> persons = GeneratePersonList(input).ToList();
+
+            List<PersonHappiness> addMyself = new();
+            foreach (PersonHappiness uniquePerson in persons.Where(uniquePerson => addMyself.All(x => x.Name != uniquePerson.Name)))
+            {
+                addMyself.Add(new PersonHappiness()
+                {
+                    Name = uniquePerson.Name,
+                    WhenNextTo = "Me",
+                    Happiness = 0
+                });
+
+                addMyself.Add(new PersonHappiness()
+                {
+                    Name = "Me",
+                    WhenNextTo = uniquePerson.Name,
+                    Happiness = 0
+                });
+            }
+
+            persons = persons.Concat(addMyself).ToList();
+
+            List<IEnumerable<string>> permutations = GetPermutations(persons);
+            return GenerateSeatMap(permutations, persons).Max(x => x.Happiness);
+        }
+
+        private static List<IEnumerable<string>> GetPermutations(List<PersonHappiness> persons)
+        {
+            List<string> uniquePersons = persons
+                .Select(x => x.Name)
+                .GroupBy(x => x)
+                .Select(x => x.First())
+                .ToList();
+
+            List<IEnumerable<string>> permutations = uniquePersons.GetPermutations().ToList();
+            return permutations;
+        }
+
+
+        public IEnumerable<PersonHappiness> GeneratePersonList(string input)
+        {
+            /*  Group 1 => Name1
+             *  Group 2 => Operation (lose/gain)
+             *  Group 3 => Numeric Value
+             *  Group 4 => Name2
+             */
+            string pattern = @"([a-zA-Z]*) would ([a-z]{4}) (\d*) .*to ([a-zA-Z]*)";
+            foreach (string row in input.Split("\r\n"))
+            {
+                Match match = Regex.Match(row, pattern);
+
+
+
+                int val = int.Parse(match.Groups[3].Value);
+                yield return new PersonHappiness()
+                {
+                    Happiness = match.Groups[2].Value == "gain" ? val : -val,
+                    Name = match.Groups[1].Value,
+                    WhenNextTo = match.Groups[4].Value
+                };
+            }
+        }
+
+
+        List<TableSeating> GenerateSeatMap(List<IEnumerable<string>> permutations, List<PersonHappiness> persons)
+        {
+            List<TableSeating> temp = new List<TableSeating>();
+            foreach (IEnumerable<string> orientation in permutations)
+            {
+                int happiness = 0;
+                List<string> personTable = orientation.ToList();
+
+                for (int i = 0; i < personTable.Count() - 1; i++)
+                {
+                    happiness += persons.First(x => x.Name == personTable[i] && x.WhenNextTo == personTable[i + 1]).Happiness;
+                    happiness += persons.First(x => x.Name == personTable[i + 1] && x.WhenNextTo == personTable[i]).Happiness;
+                }
+
+                // Add the last person
+                happiness += persons.First(x => x.Name == personTable.First() && x.WhenNextTo == personTable.Last()).Happiness;
+                happiness += persons.First(x => x.Name == personTable.Last() && x.WhenNextTo == personTable.First()).Happiness;
+
+
+
+                temp.Add(new TableSeating(personTable.ToArray(), happiness));
+            }
+
+            return temp;
+        }
+
+
+
+        public class PersonHappiness
+        {
+            public string Name { get; set; }
+            public int Happiness { get; set; }
+            public string WhenNextTo { get; set; }
+        }
+
+        private struct TableSeating
+        {
+            public string[] Persons { get; }
+            public int Happiness { get; }
+
+            public TableSeating(string[] persons, int happiness)
+            {
+                Persons = persons;
+                Happiness = happiness;
+            }
+        }
+    }
+}
---
+++ Source/Aoc.Tests/Y2015/Day13.cs
@@ -0,0 +1,47 @@
+﻿using AoC.Solutions.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC.Solutions;
+
+namespace Aoc.Tests.Y2015
+{
+    public class Day13 : DefaultAoCDay
+    {
+
+        public Day13() : base(File.ReadAllText("Inputs\\Y2015D13.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2015.D13();
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            string input =
+                "Alice would gain 54 happiness units by sitting next to Bob.\r\n" +
+                "Alice would lose 79 happiness units by sitting next to Carol.\r\n" +
+                "Alice would lose 2 happiness units by sitting next to David.\r\n" +
+                "Bob would gain 83 happiness units by sitting next to Alice.\r\n" +
+                "Bob would lose 7 happiness units by sitting next to Carol.\r\n" +
+                "Bob would lose 63 happiness units by sitting next to David.\r\n" +
+                "Carol would lose 62 happiness units by sitting next to Alice.\r\n" +
+                "Carol would gain 60 happiness units by sitting next to Bob.\r\n" +
+                "Carol would gain 55 happiness units by sitting next to David.\r\n" +
+                "David would gain 46 happiness units by sitting next to Alice.\r\n" +
+                "David would lose 7 happiness units by sitting next to Bob.\r\n" +
+                "David would gain 41 happiness units by sitting next to Carol.";
+
+            int ans = (int)Puzzle.SolvePuzzleA(input);
+            Assert.IsTrue(ans == 330, $"Answer for example was invalid, expected 330 got {ans}");
+
+        }
+
+        [Test]
+        public override void SampleTestB()
+        { }
+    }
+}
