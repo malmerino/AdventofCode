9d4f6d6fa05aa57539d67afa0cf38fa390889584
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-11-15 00:30:00 +0100

    Added classes and input for Day 15

---
+++ Source/AoC.Solutions/Y2015/D15.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using SpaceStation.Utilities.LINQ;
+
+namespace AoC.Solutions.Y2015
+{
+    public class D15 : AoCPuzzle
+    {
+        public D15() : base(2015, 15)
+        { }
+        
+
+
+        public override object SolvePuzzleA(string input)
+        {
+            return "";
+
+        }
+        public override object SolvePuzzleB(string input)
+        {
+            return "";
+
+        }
+    }
+}
---
+++ Source/Aoc.Tests/Y2015/Day15.cs
@@ -0,0 +1,36 @@
+﻿using AoC.Solutions.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using AoC.Solutions;
+using static AoC.Solutions.Y2015.D14;
+
+namespace Aoc.Tests.Y2015
+{
+    public class Day15 : DefaultAoCDay
+    {
+
+        public Day15() : base(File.ReadAllText("Inputs\\Y2015D15.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2015.D15();
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+          
+
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+        
+        }
+    }
+}
