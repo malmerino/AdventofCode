e9ba69c78adc68d9bd50bdfa401cbe1021f43eee
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-11-15 00:27:14 +0100

    Finished Day 14

---
+++ Source/AoC.Solutions/Y2015/D14.cs
@@ -0,0 +1,141 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using SpaceStation.Utilities.LINQ;
+using static AoC.Solutions.Y2015.D14;
+
+namespace AoC.Solutions.Y2015
+{
+    public class D14 : AoCPuzzle
+    {
+        public D14() : base(2015, 14)
+        { }
+        
+
+
+        public override object SolvePuzzleA(string input)
+        {
+            List<Reindeer> reindeer = GenerateReindeerList(input).ToList();
+
+            List<ReindeerSimulation> reindeerSimulation = reindeer.Select(reindeer1 => new ReindeerSimulation()
+            {
+                Reindeer = reindeer1
+            }).ToList();
+
+            reindeerSimulation.ForEach(SetupReindeer);
+
+
+            for (int i = 0; i < 2503; i++)
+            {
+                reindeerSimulation.ForEach(UpdateReindeer);
+            }
+            
+
+            return reindeerSimulation.Max(x => x.DistanceFromStart);
+
+        }
+        public override object SolvePuzzleB(string input)
+        {
+            List<Reindeer> reindeer = GenerateReindeerList(input).ToList();
+
+            List<ReindeerSimulation> reindeerSimulation = reindeer.Select(reindeer1 => new ReindeerSimulation()
+            {
+                Reindeer = reindeer1
+            }).ToList();
+
+            reindeerSimulation.ForEach(SetupReindeer);
+
+
+            for (int i = 0; i < 2503; i++)
+            {
+                reindeerSimulation.ForEach(UpdateReindeer);
+                reindeerSimulation.OrderByDescending(x => x.DistanceFromStart).First().Score++;
+
+            }
+            
+            return reindeerSimulation.Max(x => x.Score);
+
+        }
+        
+
+        public IEnumerable<Reindeer> GenerateReindeerList(string input)
+        {
+            string pattern = @"([A-Z]{1}[a-z]*) can fly (\d*) km/s for (\d*).* for (\d*)";
+
+            foreach (string row in input.Split("\r\n"))
+            {
+                /*  Group 1 => Name
+                 *  Group 2 => Speed KM/S
+                 *  Group 3 => Duration
+                 *  Group 4 => Rest
+                 */
+                Match match = Regex.Match(row, pattern);
+
+                int speed = int.Parse(match.Groups[2].Value);
+                int duration = int.Parse(match.Groups[3].Value);
+                int rest = int.Parse(match.Groups[4].Value);
+
+                yield return new Reindeer()
+                {
+                    Name = match.Groups[1].Value,
+                    Speed = speed,
+                    Duration = duration,
+                    Rest = rest
+                };
+            }
+
+
+        }
+
+
+        public void UpdateReindeer(ReindeerSimulation reindeer)
+        {
+            if (reindeer.RunTimer > 0)
+            {
+                reindeer.DistanceFromStart += reindeer.Reindeer.Speed;
+                reindeer.RunTimer--;
+            }
+            else if (reindeer.RestTimer > 0)
+            {
+                reindeer.RestTimer--;
+            }
+            else
+            {
+                reindeer.RunTimer = reindeer.Reindeer.Duration;
+                reindeer.RestTimer = reindeer.Reindeer.Rest;
+                UpdateReindeer(reindeer);
+            }
+
+        }
+        public void SetupReindeer(ReindeerSimulation reindeer)
+        {
+            reindeer.RunTimer = reindeer.Reindeer.Duration;
+            reindeer.RestTimer = reindeer.Reindeer.Rest;
+        }
+
+
+        public class Reindeer
+        {
+            public string Name { get; set; }
+            public int Speed { get; set; }
+            public int Duration { get; set; }
+            public int Rest { get; set; }
+        }
+
+
+        public class ReindeerSimulation
+        {
+            public Reindeer Reindeer { get; set; }
+
+            public int DistanceFromStart { get; set; }
+
+            public int RestTimer { get; set; }
+
+            public int RunTimer { get; set; }
+
+            public int Score { get; set; }
+
+        }
+    }
+}
---
+++ Source/Aoc.Tests/Y2015/Day14.cs
@@ -0,0 +1,92 @@
+﻿using AoC.Solutions.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using AoC.Solutions;
+using static AoC.Solutions.Y2015.D14;
+
+namespace Aoc.Tests.Y2015
+{
+    public class Day14 : DefaultAoCDay
+    {
+
+        public Day14() : base(File.ReadAllText("Inputs\\Y2015D14.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2015.D14();
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            if (Puzzle is D14 puzzle)
+            {
+                string input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\r\n" +
+                               "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+
+
+                List<D14.Reindeer> reindeer = (Puzzle as D14).GenerateReindeerList(input).ToList();
+
+                List<D14.ReindeerSimulation> reindeerSimulation = reindeer.Select(reindeer1 =>
+                    new D14.ReindeerSimulation()
+                    {
+                        Reindeer = reindeer1
+                    }).ToList();
+
+
+                reindeerSimulation.ForEach(puzzle.SetupReindeer);
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    reindeerSimulation.ForEach(puzzle.UpdateReindeer);
+                }
+
+                int comet = reindeerSimulation.First(x => x.Reindeer.Name == "Comet").DistanceFromStart;
+                int dancer = reindeerSimulation.First(x => x.Reindeer.Name == "Dancer").DistanceFromStart;
+
+                Assert.IsTrue(comet == 1120, $"Comet was expected to get 1120 but got {comet}");
+                Assert.IsTrue(dancer == 1056, $"Dancer was expected to get 1056 but got {dancer}");
+            }
+
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            if (Puzzle is D14 puzzle)
+            {
+                string input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\r\n" +
+                               "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+
+
+                List<D14.Reindeer> reindeer = (Puzzle as D14).GenerateReindeerList(input).ToList();
+
+                List<D14.ReindeerSimulation> reindeerSimulation = reindeer.Select(reindeer1 =>
+                    new D14.ReindeerSimulation()
+                    {
+                        Reindeer = reindeer1
+                    }).ToList();
+
+
+                reindeerSimulation.ForEach(puzzle.SetupReindeer);
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    reindeerSimulation.ForEach(puzzle.UpdateReindeer);
+                    reindeerSimulation.OrderByDescending(x => x.DistanceFromStart).First().Score++;
+
+                }
+
+                int comet = reindeerSimulation.First(x => x.Reindeer.Name == "Comet").Score;
+                int dancer = reindeerSimulation.First(x => x.Reindeer.Name == "Dancer").Score;
+
+                Assert.IsTrue(comet == 312, $"Comet was expected to get 312 but got {comet}");
+                Assert.IsTrue(dancer == 688, $"Dancer was expected to get 688 but got {dancer}");
+            }
+        }
+    }
+}
