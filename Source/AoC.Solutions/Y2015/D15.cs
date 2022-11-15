687cb9b3602dd65dc61a9f3d81fc34c372461db0
Eric Malmerstrom <eric.malmerstrom@veoneer.com>
2022-11-15 09:14:41 +0100

    Added Parser for day 15

--- Source/AoC.Solutions/Y2015/D15.cs
+++ Source/AoC.Solutions/Y2015/D15.cs
@@ -10,7 +10,7 @@ namespace AoC.Solutions.Y2015
     {
         public D15() : base(2015, 15)
         { }
-        
+
 
 
         public override object SolvePuzzleA(string input)
@@ -23,5 +23,41 @@ namespace AoC.Solutions.Y2015
             return "";
 
         }
+
+
+
+        private IEnumerable<Ingredient> ParseInput(string input)
+        {
+            return from row in input.Split("\r\n")
+                let pattern = @"([A-Z]{1}[a-z]*): capacity (-\d{1}|\d{1}), durability (-\d{1}|\d{1}), flavor (-\d{1}|\d{1}), texture (-\d{1}|\d{1}), calories (-\d{1}|\d{1})"
+                select Regex.Match(row, pattern)
+                into match
+                let capacity = int.Parse(match.Groups[2].Value)
+                let durability = int.Parse(match.Groups[3].Value)
+                let flavor = int.Parse(match.Groups[4].Value)
+                let texture = int.Parse(match.Groups[5].Value)
+                let calories = int.Parse(match.Groups[6].Value)
+                select new Ingredient()
+            {
+                Calories = calories,
+                Texture = texture,
+                Flavor = flavor,
+                Durability = durability,
+                Capacity = capacity,
+                Name = match.Groups[1].Value,
+            };
+        }
+
+
+        private class Ingredient
+        {
+            public string Name { get; set; }
+            public int Capacity { get; set; }
+            public int Durability { get; set; }
+            public int Flavor { get; set; }
+            public int Texture { get; set; }
+            public int Calories { get; set; }
+        }
+
     }
 }
