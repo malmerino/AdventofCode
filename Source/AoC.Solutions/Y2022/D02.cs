18953c70cd00e2e0d9498bff8eb37597baed3734
Eric Malmerstrom <eric.malmerstrom@veoneer.com>
2022-12-02 11:34:45 +0100

    Solved 2022 Day 2

---
+++ Source/AoC.Solutions/Y2022/D02.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Solutions.Y2022
+{
+    public class D02 : AoCPuzzle
+    {
+        public D02() : base(2022, 02) { }
+
+        public override object SolvePuzzleA(string input)
+        {
+            List<Round> gameResults = GetRoundInfoPuzzleA(input).ToList();
+
+            return gameResults.Sum(round => (int)GetResultForRound(round) + (int)round.Player);
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            List<Round> gameResults = GetRoundInfoPuzzleB(input).ToList();
+
+            return gameResults.Sum(round => (int)GetResultForRound(round) + (int)round.Player);
+        }
+
+
+        private static IEnumerable<Round> GetRoundInfoPuzzleA(string input)
+        {
+            string[] rows = input.Split("\r\n");
+            foreach (string row in rows)
+            {
+                string[] instructions = row.Split(' ');
+
+                char player = instructions[1][0];
+                char opponent = instructions[0][0];
+
+                Play playersPlay = TranslateForPuzzleA(player);
+                Play opponentsPlay = TranslateForPuzzleA(opponent);
+
+                yield return new Round(playersPlay, opponentsPlay);
+            }
+        }
+
+        private static IEnumerable<Round> GetRoundInfoPuzzleB(string input)
+        {
+            string[] rows = input.Split("\r\n");
+            foreach (string row in rows)
+            {
+                string[] instructions = row.Split(' ');
+
+                char player = instructions[1][0];
+                char opponent = instructions[0][0];
+
+                Play opponentsPlay = TranslateForPuzzleA(opponent);
+                Play playersPlay = GetCounterPlayForPuzzleB(player, opponentsPlay);
+
+                yield return new Round(playersPlay, opponentsPlay);
+            }
+        }
+
+        private static Play TranslateForPuzzleA(char move) => move switch
+        {
+            'A' or 'X' => Play.Rock,
+            'B' or 'Y' => Play.Paper,
+            'C' or 'Z' => Play.Scissors,
+            _ => throw new Exception($"Instruction for char \"{move}\" is unknown")
+        };
+
+
+        //X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+        private static Play GetCounterPlayForPuzzleB(char result, Play opponent)
+        {
+            return result switch
+            {
+                // Lose
+                'X' => opponent switch
+                {
+                    Play.Rock => Play.Scissors,
+                    Play.Paper => Play.Rock,
+                    Play.Scissors => Play.Paper,
+                    _ => throw new Exception("Unknown Play")
+                },
+                // Draw
+                'Y' => opponent,
+                // Win
+                'Z' => opponent switch
+                {
+                    Play.Rock => Play.Paper,
+                    Play.Paper => Play.Scissors,
+                    Play.Scissors => Play.Rock,
+                    _ => throw new Exception("Unknown Play")
+                },
+                _ => throw new Exception("Unknown Instruction")
+            };
+        }
+
+
+        private static Result GetResultForRound(Round round)
+        {
+            return round.Player switch
+            {
+                Play.Paper => round.Opponent switch
+                {
+                    Play.Paper => Result.Draw,
+                    Play.Scissors => Result.Loss,
+                    Play.Rock => Result.Win,
+                    _ => throw new Exception("Unknown Play")
+                },
+                Play.Rock => round.Opponent switch
+                {
+                    Play.Paper => Result.Loss,
+                    Play.Scissors => Result.Win,
+                    Play.Rock => Result.Draw,
+                    _ => throw new Exception("Unknown Play")
+                },
+                Play.Scissors => round.Opponent switch
+                {
+                    Play.Paper => Result.Win,
+                    Play.Scissors => Result.Draw,
+                    Play.Rock => Result.Loss,
+                    _ => throw new Exception("Unknown Play")
+                },
+                _ => throw new Exception("Unknown Instruction")
+            };
+        }
+
+        private record Round(Play Player, Play Opponent);
+
+        private enum Play
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3
+        }
+
+        private enum Result
+        {
+            Loss = 0,
+            Draw = 3,
+            Win = 6,
+        }
+
+    }
+}
---
+++ Source/Aoc.Tests/Y2022/Day02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Y2022
+{
+    public class Day02 : DefaultTestAoCDay
+    {
+        private string sampleInput;
+        public Day02() : base(File.ReadAllText("Inputs\\Y2022D02.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2022.D02();
+            sampleInput = "A Y\r\nB X\r\nC Z";
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            int ans = (int)Puzzle.SolvePuzzleA(sampleInput);
+            Assert.IsTrue(ans == 15, $"Expected 15 and got {ans}");
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            int ans = (int)Puzzle.SolvePuzzleB(sampleInput);
+            Assert.IsTrue(ans == 12, $"Expected 12 and got {ans}");
+        }
+    }
+}
