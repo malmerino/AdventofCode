9d90f99ada19ca1045f1589b481823ad1504f2dd
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-03 19:40:20 +0100

    Solved Day 3 2022

---
+++ Source/AoC.Solutions/Y2022/D03.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ReSharper disable StringLiteralTypo
+
+namespace AoC.Solutions.Y2022
+{
+    public class D03 : AoCPuzzle
+    {
+        public D03() : base(2022, 03) { }
+
+        public override object SolvePuzzleA(string input)
+        {
+            IEnumerable<Rucksack> rucksacks = GenerateRucksacks(input);
+            return GetPriorities(rucksacks);
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            IEnumerable<Rucksack> rucksacks = GenerateRucksacks(input);
+            IEnumerable<char> badges = GetBadges(rucksacks.ToArray());
+            return badges.Sum(x => Alphabet.IndexOf(x) + 1);
+        }
+        
+        private static IEnumerable<Rucksack> GenerateRucksacks(string input)
+        {
+            string[] rows = input.Split("\r\n");
+
+            foreach (string rucksack in rows)
+            {
+                string compartment1 = rucksack[..(rucksack.Length / 2)];
+                string compartment2 = rucksack[(rucksack.Length / 2)..];
+
+                if (compartment1.Length != compartment2.Length)
+                {
+                    throw new Exception("Input is not dividable by 2 without reminder");
+                }
+
+                yield return new Rucksack(rucksack, compartment1, compartment2);
+            }
+        }
+
+        private static int GetPriorities(IEnumerable<Rucksack> rucksacks)
+        {
+            return rucksacks.Select(rucksack => rucksack.Compartment1.First(x => rucksack.Compartment2.Contains(x)))
+                .Select(commonItem => Alphabet.IndexOf(commonItem) + 1)
+                .Sum();
+        }
+
+        private static IEnumerable<char> GetBadges(Rucksack[] rucksacks)
+        {
+            for (int i = 0; i < rucksacks.Count(); i += 3)
+            {
+                Rucksack[] group = rucksacks[i..(i + 3)];
+                foreach (char t1 in Alphabet)
+                {
+                    bool success = false;
+                    foreach (Rucksack t in group)
+                    {
+                        if (!t.Combined.Contains(t1))
+                        {
+                            success = false;
+                            break;
+                        }
+                        success = true;
+                    }
+
+                    if(!success) continue;
+                    yield return t1;
+                }
+            }
+        }
+        
+        private record Rucksack(string Combined, string Compartment1, string Compartment2);
+
+
+        private static string Alphabet => @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    }
+}
---
+++ Source/Aoc.Tests/Y2022/Day03.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Y2022
+{
+    public class Day03 : DefaultTestAoCDay
+    {
+        private string sampleInput;
+        public Day03() : base(File.ReadAllText("Inputs\\Y2022D03.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2022.D03();
+            sampleInput = "vJrwpWtwJgWrhcsFMMfFFhFp\r\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\r\nPmmdzqPrVvPwwTWBwg\r\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\r\nttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            int ans = (int)Puzzle.SolvePuzzleA(sampleInput);
+            Assert.IsTrue(ans == 157, $"Expected 157 and got {ans}");
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            int ans = (int)Puzzle.SolvePuzzleB(sampleInput);
+            Assert.IsTrue(ans == 70, $"Expected 70 and got {ans}");
+        }
+    }
+}
