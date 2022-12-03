cd7f4c76515f2b3c55bdbaf98a42550e35f9a943
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-04 00:08:49 +0100

    Started working on Day 17 2015

---
+++ Source/AoC.Solutions/Y2015/D17.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC.Solutions.Y2015
+{
+    public class D17 : AoCPuzzle
+    {
+        public D17() : base(2015, 17)
+        {
+        }
+
+        public override object SolvePuzzleA(string input)
+        {
+            var containers = GetContainers(input);
+
+
+
+            return 0;
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            return 0;
+        }
+
+        private IEnumerable<int> GetContainers(string input)
+        {
+            string[] rows = input.Split("\r\n");
+
+            foreach (string row in rows)
+            {
+                yield return int.Parse(row);
+            }
+        }
+    }
+}§
---
+++ Source/Aoc.Tests/Y2015/Day17.cs
@@ -0,0 +1,35 @@
+﻿using AoC.Solutions.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using AoC.Solutions;
+
+namespace Aoc.Tests.Y2015
+{
+    public class Day17 : DefaultTestAoCDay
+    {
+        private readonly string sampleInput = "";
+
+        public Day17() : base(File.ReadAllText("Inputs\\Y2015D17.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2015.D17();
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+           // No samples available to create a test
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            // No samples available to create a test
+        }
+    }
+}
