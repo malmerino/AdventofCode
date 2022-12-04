088c6bdc4cf56794c6558b7c058d388aa61620e9
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-04 14:48:56 +0100

    Added Templetes for Day 4 2022

---
+++ Source/AoC.Solutions/Y2022/D04.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ReSharper disable StringLiteralTypo
+
+namespace AoC.Solutions.Y2022
+{
+    public class D04 : AoCPuzzle
+    {
+        public D04() : base(2022, 04) { }
+
+        public override object SolvePuzzleA(string input)
+        {
+            return 0;
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            return 0;
+        }
+
+    
+    }
+}
---
+++ Source/Aoc.Tests/Y2022/Day04.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Y2022
+{
+    public class Day04 : DefaultTestAoCDay
+    {
+        private string sampleInput;
+        public Day04() : base(File.ReadAllText("Inputs\\Y2022D04.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2022.D04();
+            sampleInput = "vJrwpWtwJgWrhcsFMMfFFhFp\r\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\r\nPmmdzqPrVvPwwTWBwg\r\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\r\nttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            int ans = (int)Puzzle.SolvePuzzleA(sampleInput);
+            Assert.IsTrue(ans == 157, $"Expected 157 and got {ans}");
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            int ans = (int)Puzzle.SolvePuzzleB(sampleInput);
+            Assert.IsTrue(ans == 70, $"Expected 70 and got {ans}");
+        }
+    }
+}
