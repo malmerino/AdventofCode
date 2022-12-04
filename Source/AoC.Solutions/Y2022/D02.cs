aeb35e573ccc50ff18aedcf02c604eb48a186165
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-04 15:45:09 +0100

    Refactoring

--- Source/AoC.Solutions/Y2022/D02.cs
+++ Source/AoC.Solutions/Y2022/D02.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AoC.Solutions.Y2022
+﻿namespace AoC.Solutions.Y2022
 {
     public class D02 : AoCPuzzle
     {
