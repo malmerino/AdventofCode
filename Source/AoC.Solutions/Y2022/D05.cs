c9a560724501ef048343af22340fc2d89889fd92
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-05 20:13:47 +0100

    Solved Day 5 2022

---
+++ Source/AoC.Solutions/Y2022/D05.cs
@@ -0,0 +1,130 @@
+﻿
+// ReSharper disable StringLiteralTypo
+
+using System.Text.RegularExpressions;
+
+namespace AoC.Solutions.Y2022
+{
+    public class D05 : AoCPuzzle
+    {
+        public D05() : base(2022, 05) { }
+
+        public override object SolvePuzzleA(string input)
+        {
+            InterpretInstructions(input, out IEnumerable<MoveInstruction> moveInstructions, out IEnumerable<Stack> stackInstructions);
+            Dictionary<int, string> stacks = RunCraneInstructions(moveInstructions, stackInstructions, false);
+
+            return stacks.Aggregate("", (sum, add) => sum + add.Value.Last());
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            InterpretInstructions(input, out IEnumerable<MoveInstruction> moveInstructions, out IEnumerable<Stack> stackInstructions);
+            Dictionary<int, string> stacks = RunCraneInstructions(moveInstructions, stackInstructions, true);
+
+            return stacks.Aggregate("", (sum, add) => sum + add.Value.Last());
+        }
+
+
+        private static Dictionary<int, string> RunCraneInstructions(IEnumerable<MoveInstruction> moveInstructions, IEnumerable<Stack> stackInstructions, bool crateMover9001)
+        {
+            Dictionary<int, string> stacks = stackInstructions.ToDictionary(s => s.Key, s => s.Values);
+
+            foreach (MoveInstruction instruction in moveInstructions)
+            {
+                if (crateMover9001)
+                {
+                    stacks[instruction.Destination] += stacks[instruction.Source][^instruction.Amount..];
+                    stacks[instruction.Source] = stacks[instruction.Source][..^instruction.Amount];
+                }
+                else
+                {
+                    for (int i = 0; i < instruction.Amount; i++)
+                    {
+                        stacks[instruction.Destination] += stacks[instruction.Source][^1..];
+                        stacks[instruction.Source] = stacks[instruction.Source][..^1];
+                    }
+                }
+            }
+
+            return stacks;
+        }
+
+
+
+
+        private static void InterpretInstructions(string input, out IEnumerable<MoveInstruction> moveInstructions, out IEnumerable<Stack> stackInstructions)
+        {
+            string[] part = input.Split("\r\n\r\n");
+
+            stackInstructions = LoadInitialStack(part[0]);
+            moveInstructions = LoadMoveInstructions(part[1]);
+        }
+
+
+        private static IEnumerable<MoveInstruction> LoadMoveInstructions(string input)
+        {
+            const string pattern = @"move (\d{1,3}) from (\d{1,3}) to (\d{1,3})";
+
+            foreach (Match match in Regex.Matches(input, pattern, RegexOptions.Multiline))
+            {
+                int amount = int.Parse(match.Groups[1].Value);
+                int source = int.Parse(match.Groups[2].Value);
+                int destination = int.Parse(match.Groups[3].Value);
+
+                yield return new MoveInstruction(amount, source, destination);
+            }
+        }
+
+        private static IEnumerable<Stack> LoadInitialStack(string input)
+        {
+            string[] rows = input.Split("\r\n");
+            char[,] grid = new char[rows[0].Length, rows.Length];
+
+
+            for (int x = 0; x < rows[0].Length; x++)
+            {
+                for (int y = 0; y < rows.Length; y++)
+                {
+                    grid[x, y] = rows[y][x];
+                }
+            }
+
+            int maxY = grid.GetLength(1) - 1;
+            for (int x = 0; x < grid.GetLength(0) - 1; x++)
+            {
+                if (grid[x, maxY] == ' ') continue;
+
+                Stack stack = new(int.Parse($"{grid[x, maxY]}"));
+
+                for (int y = maxY - 1; y >= 0; y--)
+                {
+                    if (grid[x, y] == ' ') break;
+
+                    stack.Values += grid[x, y];
+                }
+
+                yield return stack;
+            }
+        }
+
+
+
+        private record MoveInstruction(int Amount, int Source, int Destination);
+
+        private class Stack
+        {
+            public int Key { get; }
+
+            public Stack(int key)
+            {
+                Key = key;
+                Values = string.Empty;
+            }
+
+            public string Values { get; set; }
+        }
+
+
+    }
+}
---
+++ Source/Aoc.Tests/Y2022/Day05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Y2022
+{
+    public class Day05 : DefaultTestAoCDay
+    {
+        private string sampleInput;
+        public Day05() : base(File.ReadAllText("Inputs\\Y2022D05.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2022.D05();
+            sampleInput = "    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2";
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            string ans = (string)Puzzle.SolvePuzzleA(sampleInput);
+            Assert.IsTrue(ans == "CMZ", $"Expected CMZ and got {ans}");
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            string ans = (string)Puzzle.SolvePuzzleB(sampleInput);
+            Assert.IsTrue(ans == "MCD", $"Expected MCD and got {ans}");
+        }
+    }
+}
