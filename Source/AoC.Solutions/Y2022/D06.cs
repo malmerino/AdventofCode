b4aced5070ff96ea8232befa4e67371a505624e9
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-06 17:17:08 +0100

    Solved Day 6 2022

---
+++ Source/AoC.Solutions/Y2022/D06.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC.Solutions.Y2022
+{
+    public class D06 : AoCPuzzle
+    {
+        public D06() : base(2022, 06) { }
+
+        public override object SolvePuzzleA(string input)
+        {
+            return FindDistinctCharacters(4, input);
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            return FindDistinctCharacters(14, input);
+        }
+
+
+        private int FindDistinctCharacters(int distinct, string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string msg = input[i..(i + distinct)];
+
+                if (msg.GroupBy(x => x).Count() == distinct)
+                {
+                    return i + distinct;
+                }
+            }
+            throw new Exception("Four distinct characters could not be found");
+        }
+    }
+}
+
---
+++ Source/Aoc.Tests/Y2022/Day06.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ReSharper disable StringLiteralTypo
+
+namespace Aoc.Tests.Y2022
+{
+    public class Day06 : DefaultTestAoCDay
+    {
+
+        public Day06() : base(File.ReadAllText("Inputs\\Y2022D06.txt")) { }
+
+        public override void Setup()
+        {
+            Puzzle = new AoC.Solutions.Y2022.D06();
+        }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            RunSampleTestA(Puzzle, "bvwbjplbgvbhsrlpgdmjqwftvncz", 5);
+            RunSampleTestA(Puzzle, "nppdvjthqldpwncqszvftbrmjlhg", 6);
+            RunSampleTestA(Puzzle, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10);
+            RunSampleTestA(Puzzle, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11);
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            RunSampleTestB(Puzzle, "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19);
+            RunSampleTestB(Puzzle, "bvwbjplbgvbhsrlpgdmjqwftvncz", 23);
+            RunSampleTestB(Puzzle, "nppdvjthqldpwncqszvftbrmjlhg", 23);
+            RunSampleTestB(Puzzle, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29);
+            RunSampleTestB(Puzzle, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26);
+        }
+    }
+}
