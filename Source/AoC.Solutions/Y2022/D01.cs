7124704e4f431ac8f11eb201b982bca441284755
Eric Malmerstrom <eric.malmerstrom@gmail.com>
2022-12-06 17:28:38 +0100

    Fixed Day 1 2022, it did not save the last elf and therefor the B puzzle did not work

--- Source/AoC.Solutions/Y2022/D01.cs
+++ Source/AoC.Solutions/Y2022/D01.cs
@@ -14,14 +14,14 @@
             return GetElves(input).OrderByDescending(x => x.Calories).Take(3).Sum(x => x.Calories);
         }
 
-        private IEnumerable<Elf> GetElves(string input)
+        private static IEnumerable<Elf> GetElves(string input)
         {
-            var rows = input.Split("\r\n");
+            string[] rows = input.Split("\r\n");
             int calories = 0;
 
             foreach (string row in rows)
             {
-                if (string.IsNullOrWhiteSpace(row)) // expect empty row
+                if (string.IsNullOrWhiteSpace(row))
                 {
                     int cal = calories;
                     calories = 0;
@@ -29,6 +29,8 @@
                 }
                 else calories += int.Parse(row);
             }
+
+            yield return new Elf(calories);
         }
 
         private record Elf(int Calories);
