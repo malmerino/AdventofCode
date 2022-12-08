68c6b57de400cb0528580516ba90904ea6929e2e
Eric Malmerstrom <eric.malmerstrom@veoneer.com>
2022-12-08 22:03:56 +0100

    Day 8 2022 WIP

--- Source/AoC.Solutions/Y2022/D08.cs
+++ Source/AoC.Solutions/Y2022/D08.cs
@@ -5,11 +5,15 @@ namespace AoC.Solutions.Y2022
     public class D08 : AoCPuzzle
     {
         public D08() : base(2022, 08) { }
-
-       
-
+        
         public override object SolvePuzzleA(string input)
         {
+            int[,] map = GenerateMap(input);
+            
+
+
+
+
             return 0;
         }
 
@@ -17,7 +21,24 @@ namespace AoC.Solutions.Y2022
         {
             return 0;
         }
-        
+
+
+
+        private static int[,] GenerateMap(string input)
+        {
+            string[] rows = input.Split("\r\n");
+
+            int[,] mapArray = new int[rows[0].Length, rows.Length];
+
+            for (int y = 0; y < rows.Length; y++)
+            {
+                for (int x = 0; x < rows[0].Length; x++)
+                {
+                    mapArray[x, y] = int.Parse($"{rows[x][y]}");
+                }
+            }
+
+            return mapArray;
+        }
     }
 }
-
