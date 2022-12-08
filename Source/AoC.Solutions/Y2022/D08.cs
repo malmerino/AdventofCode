227df48c36127941be4119906587f6841c5ec1f4
Eric Malmerstrom <eric.malmerstrom@veoneer.com>
2022-12-08 08:15:28 +0100

    Added Templete and Inputs for Day 8 2022

---
+++ Source/AoC.Solutions/Y2022/D08.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC.Solutions.Y2022
+{
+    public class D08 : AoCPuzzle
+    {
+        public D08() : base(2022, 08) { }
+
+       
+
+        public override object SolvePuzzleA(string input)
+        {
+            return 0;
+        }
+
+        public override object SolvePuzzleB(string input)
+        {
+            return 0;
+        }
+        
+    }
+}
+
---
+++ Source/Aoc.Tests/Y2022/Day08.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ReSharper disable StringLiteralTypo
+
+namespace Aoc.Tests.Y2022
+{
+    public class Day08 : DefaultTestAoCDay
+    {
+
+        public Day08() : base(new AoC.Solutions.Y2022.D08()) { }
+
+        public override void Setup() { }
+
+        [Test]
+        public override void SampleTestA()
+        {
+            RunSampleTestA(SampleInput, 21);
+        }
+
+        [Test]
+        public override void SampleTestB()
+        {
+            RunSampleTestB(SampleInput, 0);
+        }
+    }
+}
